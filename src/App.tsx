0ab344791bc890e2f5d3a2d8280e11e9beba0bef
pengfei wang <pengfei@deepgenomics.com>
2023-02-24 11:59:48 -0500

    update indentation

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import './App.css';
 
 
 function MainForm() {
-    return (
-        <div>
-            <p>Refactor this component to a data input form</p>
-            <ul>
-                <li>A "Topic" input that takes a maximum of 50 characters. This field is required.</li>
-                <li>A "Content" multi-line input. This field is required.</li>
-                <li>A "Submit" button sends user inputs to the backend API.</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <p>Refactor this component to a data input form</p>
+      <ul>
+        <li>A "Topic" input that takes a maximum of 50 characters. This field is required.</li>
+        <li>A "Content" multi-line input. This field is required.</li>
+        <li>A "Submit" button sends user inputs to the backend API.</li>
+      </ul>
+    </div>
+  );
 }
 
 
